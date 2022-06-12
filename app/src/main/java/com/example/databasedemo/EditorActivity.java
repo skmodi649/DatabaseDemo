5f96a0487a8bcd0dc446a5c3c0bf0bc063f89af0
Suraj Kumar Modi <76468931+skmodi649@users.noreply.github.com>
2022-06-12 13:41:10 +0530

    Created the Contract class and performed the initial steps commit

--- app/src/main/java/com/example/databasedemo/EditorActivity.java
+++ app/src/main/java/com/example/databasedemo/EditorActivity.java
@@ -13,6 +13,8 @@ import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.Spinner;
 
+import com.example.databasedemo.database.PetContract;
+
 public class EditorActivity extends AppCompatActivity {
 
     /** EditText field to enter the pet's name */
@@ -69,11 +71,11 @@ public class EditorActivity extends AppCompatActivity {
                 String selection = (String) parent.getItemAtPosition(position);
                 if (!TextUtils.isEmpty(selection)) {
                     if (selection.equals(getString(R.string.gender_male))) {
-                        mGender = 1; // Male
+                        mGender = PetContract.PetEntry.GENDER_MALE; // Male
                     } else if (selection.equals(getString(R.string.gender_female))) {
-                        mGender = 2; // Female
+                        mGender = PetContract.PetEntry.GENDER_FEMALE; // Female
                     } else {
-                        mGender = 0; // Unknown
+                        mGender = PetContract.PetEntry.GENDER_UNKNOWN; // Unknown
                     }
                 }
             }
--- app/src/main/java/com/example/databasedemo/database/PetContract.java
+++ app/src/main/java/com/example/databasedemo/database/PetContract.java
@@ -1,4 +1,29 @@
 package com.example.databasedemo.database;
 
-public class PetContract {
+import android.provider.BaseColumns;
+
+// Outer class
+public final class PetContract {
+
+
+
+    // Now create a inner class for each of the tables of the database
+    public static final class PetEntry implements BaseColumns{
+
+        // Defining constants for the table name and each of the column headings
+
+        public final static String TABLE_NAME = "pets";
+        public final static String _ID = BaseColumns._ID; // The field _ID came from the BaseColumns class
+        public final static String COLUMN_PET_NAME = "name";
+        public final static String COLUMN_PET_BREED = "breed";
+        public final static String COLUMN_PET_GENDER = "gender";
+        public final static String COLUMN_PET_WEIGHT = "weight";
+
+        // Defining the constants for the gender
+
+        public final static int GENDER_UNKNOWN = 0;
+        public final static int GENDER_MALE = 1;
+        public final static int GENDER_FEMALE = 2;
+
+    }
 }
