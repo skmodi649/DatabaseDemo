59686568a95074e12347e785a5d1ddf93243314c
Suraj Kumar Modi <76468931+skmodi649@users.noreply.github.com>
2022-06-13 15:59:02 +0530

    Updated the PetContract.java class commit

--- app/src/main/java/com/example/databasedemo/database/PetContract.java
+++ app/src/main/java/com/example/databasedemo/database/PetContract.java
@@ -1,14 +1,43 @@
 package com.example.databasedemo.database;
 
+import android.net.Uri;
 import android.provider.BaseColumns;
 
 // Outer class
 public final class PetContract {
 
+    // To prevent someone from accidentally instantiating the contract class,
+    // give it an empty constructor.
+    private PetContract() {}
+
+
+    public static final String CONTENT_AUTHORITY = "com.example.databasedemo";
+
+    /**
+     * Use CONTENT_AUTHORITY to create the base of all URI's which apps will use to contact
+     * the content provider.
+     */
+    public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
+
+    /**
+     * Possible path (appended to base content URI for possible URI's)
+     * For instance, content://com.example.databasedemo/pets/ is a valid path for
+     * looking at pet data. content://com.example.databasedemo/staff/ will fail,
+     * as the ContentProvider hasn't been given any information on what to do with "staff".
+     */
+    public static final String PATH_PETS = "pets";
 
 
     // Now create a inner class for each of the tables of the database
     public static final class PetEntry implements BaseColumns{
+
+        /** The content URI to access the pet data in the provider */
+        public static final Uri CONTENT_URI = Uri.withAppendedPath(BASE_CONTENT_URI, PATH_PETS);
+
+        /**
+         * Inner class that defines constant values for the pets database table.
+         * Each entry in the table represents a single pet.
+         */
 
         // Defining constants for the table name and each of the column headings
 
@@ -24,6 +53,9 @@ public final class PetContract {
         public final static int GENDER_UNKNOWN = 0;
         public final static int GENDER_MALE = 1;
         public final static int GENDER_FEMALE = 2;
+
+
+
 
     }
 }
