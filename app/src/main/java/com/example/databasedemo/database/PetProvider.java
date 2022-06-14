5f02e3f769e1d5a6bdb1ba7caf6c05fb4cb30d5c
Suraj Kumar Modi <76468931+skmodi649@users.noreply.github.com>
2022-06-14 11:40:44 +0530

    Implemented Content Provider query() method in PetProvider.java class commit

--- app/src/main/java/com/example/databasedemo/database/PetProvider.java
+++ app/src/main/java/com/example/databasedemo/database/PetProvider.java
@@ -1,9 +1,11 @@
 package com.example.databasedemo.database;
 
 import android.content.ContentProvider;
+import android.content.ContentUris;
 import android.content.ContentValues;
 import android.content.UriMatcher;
 import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.net.Uri;
 
 
@@ -14,6 +16,11 @@ public class PetProvider extends ContentProvider {
 
     /** Tag for the log messages */
     public static final String LOG_TAG = PetProvider.class.getSimpleName();
+
+
+    /** Database helper that will provide us access to the database */
+    private PetDbHelper mDbHelper;
+
 
 
     /*
@@ -43,7 +50,7 @@ public class PetProvider extends ContentProvider {
         //integer code {@link #PET_ID}. This Uri is used to provide access to ONE single row of the
         //pets table
         //**Note : # is used for integer and * for characters
-        sUriMatcher.addURI(PetContract.CONTENT_AUTHORITY, PetContract.PATH_PETS+"/#", PET_ID);
+        sUriMatcher.addURI(PetContract.CONTENT_AUTHORITY, PetContract.PATH_PETS + "/#", PET_ID);
 
     }
 
@@ -68,7 +75,36 @@ public class PetProvider extends ContentProvider {
     @Override
     public Cursor query(Uri uri, String[] projection, String selection, String[] selectionArgs,
                         String sortOrder) {
-        return null;
+        SQLiteDatabase database = mDbHelper.getReadableDatabase();
+        Cursor cursor;
+        int match = sUriMatcher.match(uri);
+        switch (match){
+            case PETS:
+                // For the whole table
+                // just query everything given
+                cursor = database.query(PetContract.PetEntry.TABLE_NAME, projection, selection, selectionArgs, null, null, sortOrder);
+                break;
+
+            case PET_ID:
+                // For the PET_ID code, extract out the ID from the URI.
+                // For an example URI such as "content://com.example.android.pets/pets/3",
+                // the selection will be "_id=?" and the selection argument will be a
+                // String array containing the actual ID of 3 in this case.
+                //
+                // For every "?" in the selection, we need to have an element in the selection
+                // arguments that will fill in the "?". Since we have 1 question mark in the
+                // selection, we have 1 String in the selection arguments' String array.
+
+                selection = PetContract.PetEntry._ID + "=?";
+                selectionArgs = new String[] {String.valueOf(ContentUris.parseId(uri))};
+                cursor = database.query(PetContract.PetEntry.TABLE_NAME, projection, selection, selectionArgs, null, null, sortOrder);
+                break;
+
+            default:
+                throw new IllegalArgumentException("Unable to query unknown URI " + uri);
+        }
+
+        return cursor;
     }
 
     /**
