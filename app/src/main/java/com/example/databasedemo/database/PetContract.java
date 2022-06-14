a7b2d7c049f704105595d18c9671f75135a843d8
Suraj Kumar Modi <76468931+skmodi649@users.noreply.github.com>
2022-06-15 02:46:55 +0530

    Content Provider getType method implemented in PetProvider.java class, MIME type string added in the PetContract.java class commit

--- app/src/main/java/com/example/databasedemo/database/PetContract.java
+++ app/src/main/java/com/example/databasedemo/database/PetContract.java
@@ -1,5 +1,6 @@
 package com.example.databasedemo.database;
 
+import android.content.ContentResolver;
 import android.net.Uri;
 import android.provider.BaseColumns;
 
@@ -53,6 +54,20 @@ public final class PetContract {
         public final static int GENDER_UNKNOWN = 0;
         public final static int GENDER_MALE = 1;
         public final static int GENDER_FEMALE = 2;
+
+
+        /**
+         * The MIME type of the {@link #CONTENT_URI} for a list of pets.
+         */
+        public static final String CONTENT_LIST_TYPE =
+                ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_PETS;
+
+        /**
+         * The MIME type of the {@link #CONTENT_URI} for a single pet.
+         */
+        public static final String CONTENT_ITEM_TYPE =
+                ContentResolver.CURSOR_ITEM_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_PETS;
+
 
         public static boolean isValidGender(int value){
             if(value == GENDER_UNKNOWN || value == GENDER_MALE || value == GENDER_FEMALE)
