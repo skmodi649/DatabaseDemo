ee0fb2feac0b396b86b1c3f802d253b61fa186da
Suraj Kumar Modi <76468931+skmodi649@users.noreply.github.com>
2022-06-16 01:09:24 +0530

    Basic layout of the PetCursorAdapter class done commit

---
+++ app/src/main/java/com/example/databasedemo/database/PetCursorAdapter.java
@@ -0,0 +1,31 @@
+package com.example.databasedemo.database;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CursorAdapter;
+
+public class PetCursorAdapter extends CursorAdapter {
+
+
+    /**
+     * Constructs a new {@link PetCursorAdapter}.
+     *
+     * @param context The context
+     * @param c       The cursor from which to get the data.
+     */
+    public PetCursorAdapter(Context context, Cursor c) {
+        super(context, c, 0 /* flags */);
+    }
+
+    @Override
+    public View newView(Context context, Cursor cursor, ViewGroup parent) {
+        return null;
+    }
+
+    @Override
+    public void bindView(View view, Context context, Cursor cursor) {
+
+    }
+}
